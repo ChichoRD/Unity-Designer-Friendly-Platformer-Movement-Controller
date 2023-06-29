c692626c609cf89754b56896e9c5a3e97c377d69
Chicho <chichord2005@hotmail.com>
2023-06-29 21:57:37 +0200

    (Needs) Workaround for relative transform movement

--- Scripts/Set Handlers/HorizontalMovement/HorizontalMovementSetHandler3D.cs
+++ Scripts/Set Handlers/HorizontalMovement/HorizontalMovementSetHandler3D.cs
@@ -10,14 +10,22 @@ public class HorizontalMovementSetHandler3D : HorizontalMovementSetHandler
 
     public override void ApplyMovementForces()
     {
-        Vector3 projectedNormalisedVelocity = Vector3.ProjectOnPlane(_rigidbody.velocity, _getMovementPlaneNormal()).normalized;
-        Vector3 counterMovementForce = MovementInputter.MovementController.GetCounterMovementForce() * projectedNormalisedVelocity;
+        const float DEBUG_FACTOR = 0.1f;
 
         Vector3 movementForce = YZ(MovementInputter.GetMovementForceDueToInput());
         movementForce = _getRelativeMovementForce(movementForce);
+        movementForce = movementForce.magnitude * Vector3.ProjectOnPlane(movementForce, Vector3.up).normalized;
+        Debug.DrawLine(transform.position, transform.position + movementForce * DEBUG_FACTOR, Color.red);
 
-        _rigidbody.AddForce(counterMovementForce);
-        _rigidbody.AddForce(movementForce);
+        Vector3 projectedNormalisedVelocity = Vector3.ProjectOnPlane(_rigidbody.velocity, Vector3.up).normalized;
+        Vector3 counterMovementForce = MovementInputter.MovementController.GetCounterMovementForce() * projectedNormalisedVelocity;
+        Debug.DrawLine(transform.position, transform.position + counterMovementForce * DEBUG_FACTOR, Color.blue);
+
+        _rigidbody.AddForce(counterMovementForce + movementForce);
+        //_rigidbody.AddForce(movementForce);
+
+        Debug.Log(_rigidbody.velocity.magnitude);
+        //TODO - Relative transform transformation fails when looking up or down, countermovement gets way reduced while movement does not, hence speeeeed
 
         static Vector3 YZ(Vector3 vec) => new Vector3(vec.x, vec.z, vec.y);
     }
@@ -27,7 +35,9 @@ public class HorizontalMovementSetHandler3D : HorizontalMovementSetHandler
         _getMovementPlaneNormal = _relativeTransform == null ? () => Vector3.up : () => _relativeTransform.up;
         _getRelativeMovementForce = _relativeTransform == null ? (vec) => vec : (vec) => _relativeTransform.TransformDirection(vec);
 
-        MovementInputter.InitialiseController(_relativeTransform == null ? () => Vector3.ProjectOnPlane(_rigidbody.velocity, Vector3.up).magnitude : () => Vector3.ProjectOnPlane(_rigidbody.velocity, _relativeTransform.up).magnitude,
+        MovementInputter.InitialiseController(_relativeTransform == null ?
+                                                () => Vector3.ProjectOnPlane(_rigidbody.velocity, Vector3.up).magnitude :
+                                                () => Vector3.ProjectOnPlane(_rigidbody.velocity, Vector3.up/*_relativeTransform.up*/).magnitude,
                                               () => _rigidbody.mass);
     }
 
