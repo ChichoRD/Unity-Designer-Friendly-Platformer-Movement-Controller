5c9bd02249fb0ce2e080d5cc1c91405573b67ee6
Chicho <chichord2005@hotmail.com>
2023-08-11 18:15:15 +0200

    Commented out MovementHandler debugs

--- Scripts/Set Handlers/HorizontalMovement/HorizontalMovementSetHandler3D.cs
+++ Scripts/Set Handlers/HorizontalMovement/HorizontalMovementSetHandler3D.cs
@@ -10,21 +10,21 @@ public class HorizontalMovementSetHandler3D : HorizontalMovementSetHandler
 
     public override void ApplyMovementForces()
     {
-        const float DEBUG_FACTOR = 0.1f;
+        //const float DEBUG_FACTOR = 0.1f;
 
         Vector3 movementForce = YZ(MovementInputter.GetMovementForceDueToInput());
         movementForce = _getRelativeMovementForce(movementForce);
         movementForce = movementForce.magnitude * Vector3.ProjectOnPlane(movementForce, Vector3.up).normalized;
-        Debug.DrawLine(transform.position, transform.position + movementForce * DEBUG_FACTOR, Color.red);
+        //Debug.DrawLine(transform.position, transform.position + movementForce * DEBUG_FACTOR, Color.red);
 
         Vector3 projectedNormalisedVelocity = Vector3.ProjectOnPlane(_rigidbody.velocity, Vector3.up).normalized;
         Vector3 counterMovementForce = MovementInputter.MovementController.GetCounterMovementForce() * projectedNormalisedVelocity;
-        Debug.DrawLine(transform.position, transform.position + counterMovementForce * DEBUG_FACTOR, Color.blue);
+        //Debug.DrawLine(transform.position, transform.position + counterMovementForce * DEBUG_FACTOR, Color.blue);
 
         _rigidbody.AddForce(counterMovementForce + movementForce);
         //_rigidbody.AddForce(movementForce);
 
-        Debug.Log(_rigidbody.velocity.magnitude);
+        //Debug.Log(_rigidbody.velocity.magnitude);
         //TODO - Relative transform transformation fails when looking up or down, countermovement gets way reduced while movement does not, hence speeeeed
 
         static Vector3 YZ(Vector3 vec) => new Vector3(vec.x, vec.z, vec.y);
